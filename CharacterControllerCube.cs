1473c861a1a93124321cef2487927d75cd95769e
AaronJoseCabreraMartin <alu0101101019@ull.edu.es>
2020-10-25 12:01:28 +0000

    2b

--- CharacterControllerCube.cs
+++ CharacterControllerCube.cs
@@ -7,16 +7,18 @@ using UnityEngine.UI;
 public class CharacterControllerCube : MonoBehaviour
 {
     private Transform tf_;
+    public float velocity_;
     // Start is called before the first frame update
     void Start()
     {
         tf_ = GetComponent<Transform>();
+        velocity_ = 1.5F;
     }
 
     // Update is called once per frame
     void Update()
     {
-            tf_.Translate( Vector3.forward * Time.deltaTime);
+            tf_.Translate( velocity_ * Vector3.forward * Time.deltaTime);
             Debug.Log("up");
     }
 }
